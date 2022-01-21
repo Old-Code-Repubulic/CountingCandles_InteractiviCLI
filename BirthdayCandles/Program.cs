f2cde4597e328521e85f7498ea1aa435f424aacd
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-20 20:10:28 -0600

    Add project files.

---
+++ BirthdayCandles/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Happy Birthday!");
+Console.WriteLine("How Old are you?!");
+
+string age = Console.ReadLine();
+Console.WriteLine("You are "+ age + " years old?");
