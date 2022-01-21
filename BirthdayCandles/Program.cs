2ca5a9f436ec9a52b8e21987b92f718fbf51e327
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-20 20:28:36 -0600

    updated main() to house a while loop to continue dialogue

--- BirthdayCandles/Program.cs
+++ BirthdayCandles/Program.cs
@@ -1,25 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information        
 
+Main();
 
-
-
-Console.WriteLine("Happy Birthday!");
-Console.WriteLine("How Old are you?!");
-
-string age = Console.ReadLine();
-Console.WriteLine("You are "+ age + " years old? (y/n)");
-
-string confirmation = Console.ReadLine();
-
-switch (confirmation)
+static void Main()
 {
-    case "y":
-        Console.WriteLine("this will soon house a method");
-        break;
-    case "n":
-        Console.WriteLine("I'm Sorry, what was your age?");
-        break;
-    default: Console.WriteLine("I don't know what you just put, but it is NOT in a 'y' or 'n' format!");
-        break;
+
+    bool loopKillswitch = false;
+    while (loopKillswitch == false)
+    {
+
+        Console.WriteLine("Happy Birthday!");
+        Console.WriteLine("How Old are you?!");
+
+        int age = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("You are " + age + " years old? (y/n)");
+
+        string confirmation = Console.ReadLine();
+
+        switch (confirmation)
+        {
+            case "y":
+                Console.WriteLine("this will soon house a method");
+                break;
+            case "n":
+                Console.WriteLine("I'm Sorry, what was your age?");
+                break;
+            default:
+                Console.WriteLine("I don't know what you just put, but it is NOT in a 'y' or 'n' format!");
+                break;
+        }
+    }
+
+    static void CountCandles(int age)
+    {
+        int[] 
+    }
 }
-        
