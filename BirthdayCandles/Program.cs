d47928d45acd07a9949edfda178db203f83b829c
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-20 20:16:06 -0600

    setup initial dialogue and a switch to handle responses from user

--- BirthdayCandles/Program.cs
+++ BirthdayCandles/Program.cs
@@ -7,4 +7,19 @@ Console.WriteLine("Happy Birthday!");
 Console.WriteLine("How Old are you?!");
 
 string age = Console.ReadLine();
-Console.WriteLine("You are "+ age + " years old?");
+Console.WriteLine("You are "+ age + " years old? (y/n)");
+
+string confirmation = Console.ReadLine();
+
+switch (confirmation)
+{
+    case "y":
+        Console.WriteLine("this will soon house a method");
+        break;
+    case "n":
+        Console.WriteLine("I'm Sorry, what was your age?");
+        break;
+    default: Console.WriteLine("I don't know what you just put, but it is NOT in a 'y' or 'n' format!");
+        break;
+}
+        
