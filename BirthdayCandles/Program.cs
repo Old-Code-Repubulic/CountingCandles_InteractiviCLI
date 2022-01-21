283fcbfd2b8b111d748ca78c7a4269b0ef53c3bd
joseph tidwell <joseph.tidwell09@gmail.com>
2022-01-20 20:32:46 -0600

    added to lower() on confirmation string, created new method dialogue() to be called instead of putting all loging in main method

--- BirthdayCandles/Program.cs
+++ BirthdayCandles/Program.cs
@@ -4,35 +4,40 @@ Main();
 
 static void Main()
 {
+    Console.WriteLine("Happy Birthday!");
+    dialogue();
 
-    bool loopKillswitch = false;
-    while (loopKillswitch == false)
-    {
-
-        Console.WriteLine("Happy Birthday!");
-        Console.WriteLine("How Old are you?!");
-
-        int age = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("You are " + age + " years old? (y/n)");
-
-        string confirmation = Console.ReadLine();
-
-        switch (confirmation)
+    void dialogue() {
+        bool loopKillswitch = false;
+        while (loopKillswitch == false)
         {
-            case "y":
-                Console.WriteLine("this will soon house a method");
-                break;
-            case "n":
-                Console.WriteLine("I'm Sorry, what was your age?");
-                break;
-            default:
-                Console.WriteLine("I don't know what you just put, but it is NOT in a 'y' or 'n' format!");
-                break;
+
+
+            Console.WriteLine("How Old are you?!");
+
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old? (y/n)");
+
+            string confirmation = Console.ReadLine().ToLower();
+
+            switch (confirmation)
+            {
+                case "y":
+                    Console.WriteLine("this will soon house a method");
+                    break;
+                case "n":
+                    Console.WriteLine("I'm Sorry, what was your age?");
+                    break;
+                default:
+                    Console.WriteLine("I don't know what you just put, but it is NOT in a 'y' or 'n' format!");
+                    loopKillswitch = true;
+                    break;
+            }
         }
     }
 
-    static void CountCandles(int age)
-    {
-        int[] 
-    }
+    //static void CountCandles(int age)
+    //{
+    //    int[] 
+    //}
 }
